32806f7c8d441923d7d0c36b4c524e5be3b5461b
Luiz Bills <luizpbills@gmail.com>
2018-02-12 17:29:24 +0000

    added css prefixes

--- inc/tachyons-tweaks.php
+++ inc/tachyons-tweaks.php
@@ -1,10 +1,17 @@
 <?php
 
-add_filter( WP_Modular_CSS::PREFIX . 'css_module_output_skins', 'wp_module_css_tachyons_tweaks_skins', 10, 3 );
-function wp_module_css_tachyons_tweaks_skins ( $module_content, $prefix, $module_name ) {
+add_filter( WP_Modular_CSS::PREFIX . 'css_module_output_skins', function ( $module_content, $prefix, $module_name ) {
 
 	// rename .inherit to .color-inherit
 	$module_content = str_replace( ".${prefix}inherit", ".${prefix}color-inherit", $module_content );
-
 	return $module_content;
-}
+
+}, 10, 3 );
+
+add_filter( WP_Modular_CSS::PREFIX . 'css_module_output_font-family', function ( $module_content, $prefix, $module_name ) {
+
+	// rename .inherit to .color-inherit
+	$module_content = str_replace( ".${prefix}code", "code, .${prefix}code", $module_content );
+	return $module_content;
+
+}, 10, 3 );
